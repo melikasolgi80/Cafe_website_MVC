fe0c42540d272758e21d9fa66fedc72af3ec0c16
melikaSolgi <melikasolgi80@outlook.com>
2023-04-04 20:22:12 +0430

    the assets folder, layout.cshtml and index.cshtml codes added to the project

---
+++ Cafe_MVC/wwwroot/assets/Style/Js.js
@@ -0,0 +1,32 @@
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  $('.carousel').carousel()
+
+  
+  var swiper = new Swiper(".swiperbtn", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+ 
+  });
